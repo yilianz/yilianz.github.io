4a0679ddb35c015d375015f6d40b994abbceda8d
Yilian Zhang <yilianz@usca.edu>
2019-08-04 20:38:43 -0400

    The whole layout is established . black white theme so far

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+
+/*
+document.querySelector('nav a').addEventListener('click',
+function(){
+    alert("here");
+    var content = this.textContent;
+    var E = document.getElementsByTagName('aside');
+    E.textContent = content;
+   
+});
+
+*/
+
+//update the side bar and main content
+
+$('nav a').click(function(){
+    var content= $(this).html();
+    var links = $(this).attr('href');
+    $('aside').fadeOut(400,function(){
+        $('aside').html(content);
+        $('aside').fadeIn(3000);
+    });
+      
+    $('main div').hide();
+    $(links).show();
+});
+
